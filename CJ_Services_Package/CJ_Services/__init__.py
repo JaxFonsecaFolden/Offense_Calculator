c1deeb27aebc6c26daac076dff15a7c592f224b8
Jax Fonseca-Folden <jax.ff17@gmail.com>
2025-11-06 13:19:09 -0500

    Delete CJ_Services_Package directory

--- CJ_Services_Package/CJ_Services/__init__.py
+++
@@ -1,1 +0,0 @@
-from .CJ_Services import DataSource
--- CJ_Services_Package/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='CJ_Services', # This is the package name you'll use to import
-    version='0.1.0',
-    packages=find_packages(), # Automatically finds the 'cj_services' folder
-)
